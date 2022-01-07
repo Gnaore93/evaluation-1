470bd6bc28b2218deb120c2c365cc6c0e84d4621
iMAC_33 <imac_33@192.168.50.2>
2022-01-07 13:09:59 +0000

    premier devoir

---
+++ main.py
@@ -0,0 +1,29 @@
+print("exercice 1")
+
+def etrait_ensemble_des_voyelles(richard):
+    nom = "richard"
+    liste_voyelle = ["a","e","i","o","y"]
+    
+    for i in nom:
+     if i in liste_voyelle:
+        return i;
+    pass
+
+
+
+print("exercice 2")
+
+
+   
+l = "richard"
+n = []
+for x in l:
+    n.append(ord(x))
+print(n)
+ 
+    
+
+
+    
+
+        
